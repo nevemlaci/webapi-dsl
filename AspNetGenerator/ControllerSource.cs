76f5b6bc5b0a2c61bad442384f72eab603e47cc5
Nevemlaci <nevemlaci+github@proton.me>
2026-03-04 14:03:32 +0100

    Added dto generation for update so updates have all fields as optional

--- AspNetGenerator/ControllerSource.cs
+++ AspNetGenerator/ControllerSource.cs
@@ -58,7 +58,7 @@ public class ControllerSource
             ReturnType = "IActionResult"
         };
         update.Parameters.Add(("Guid", "id"));
-        update.Parameters.Add((source.DtoClassName, "dto"));
+        update.Parameters.Add(("Update" + source.DtoClassName, "dto"));
         source.Actions.Add(update);
         
         var delete = new ActionSource() {
